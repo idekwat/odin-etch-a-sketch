a961301f4ae7977bc1a9ac83f9d31218918427ff
idekwat <184945414+idekwat@users.noreply.github.com>
2025-01-28 01:41:26 +0800

    Fixed grid to show proper 16x16 size

--- js-script.js
+++ js-script.js
@@ -1,16 +1,31 @@
 //script here
 
-const gridsHere = document.querySelector(".gridContainer");
-const size = 16;
+const container = document.querySelector(".gridContainer");
 
-function makeGrid() {
-    for(let a=0;a<size;a++) {
-        for(let b=0; b<size;b++) {
-            const createGrid = document.createElement("div");
-            createGrid.textContent = "A = " + a + "\nB = " + b;
-            gridsHere.appendChild(createGrid);
-            console.log("A = " + a + ", B = " + b);
+const sizeInput = document.querySelector('input');
+const submitButton = document.querySelector('button');
+
+const size = 16;
+/*
+submitButton.onclick = (event) => {
+    const size = sizeInput.value;
+    sizeInput.value = '';
+    createGrid(size);
+} */
+
+function createGrid(size) {
+    for(let a = 0; a < size; a++) {
+        let createRow = document.createElement("div");
+        container.appendChild(createRow).className = "rowGuide";
+    }
+    let createRow =  document.getElementsByClassName("rowGuide")
+
+    for(let b = 0; b < createRow.length; b++) {
+        for(let c = 0; c < size; c++) {
+            let newSquare = document.createElement("div");
+            createRow[b].appendChild(newSquare).className = "fullGrid";
         }
     }
 }
-makeGrid();
+
+createGrid(size);
