30dabbe3c360f23d36ae60154e9a4f0d74375e30
idekwat <184945414+idekwat@users.noreply.github.com>
2025-01-27 16:15:44 +0800

    Added code to generate grids

--- js-script.js
+++ js-script.js
@@ -1,1 +1,16 @@
 //script here
+
+const gridsHere = document.querySelector(".gridContainer");
+const size = 16;
+
+function makeGrid() {
+    for(let a=0;a<size;a++) {
+        for(let b=0; b<size;b++) {
+            const createGrid = document.createElement("div");
+            createGrid.textContent = "A = " + a + "\nB = " + b;
+            gridsHere.appendChild(createGrid);
+            console.log("A = " + a + ", B = " + b);
+        }
+    }
+}
+makeGrid();
